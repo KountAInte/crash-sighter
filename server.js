2e7c08ff62dc15207fe6b90223a0f4da54fc566a
KountAInte <drudgecode@gmail.com>
2018-07-22 18:28:11 +0200

    cambios para en seridor

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const path = require('path');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+// path location strategy
+app.get('/*', function(req,res) {    
+
+res.sendFile(path.join(__dirname+'/dist/index.html'));
+})
+
 console.log('BEEP BEEP BOOP');
